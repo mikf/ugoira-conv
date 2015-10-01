cd342d0410263fbd18c871d1951da494d0b633aa
Mike Fährmann <mike_faehrmann@web.de>
2015-10-02 00:42:45 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name="ugoira_conv",
+    version="0.1",
+    description="ugoira converter",
+    long_description="convert ugoira (pixiv animations) to webm or gif",
+    url="https://github.com/mikf/ugoira-conv",
+    author="Mike Fährmann",
+    author_email="mike_faehrmann@web.de",
+    license="GPLv2",
+    scripts=[
+        "bin/ugoira-conv",
+    ],
+    entry_points={
+        'console_scripts': [
+            'ugoira-conv = ugoira_conv:main',
+        ],
+    },
+    packages=[
+        "ugoira_conv",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Multimedia :: Video :: Conversion",
+    ],
+)
